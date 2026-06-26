38a686d66d0c79ed7a5f24f3795ec1463891d291
Leonardo-Rodrigues <leofrrodrigues86@gmail.com>
2026-06-26 09:19:19 -0300

    Aumentando o nivel ciclomático

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,9 @@ export function calculate(a: number, b: number, c: number, d: number) {
     if (b > 0) {
       if (c > 0) {
         if (d > 0) {
-          return true;
+          if (a + b + c + d > 100) {
+            return true;
+          }
         }
       }
     }
